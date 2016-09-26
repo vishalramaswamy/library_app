f299bea08323544481a0a2b830b6b5ad47b0c5d6
shyam-ramakrishnan <r.shyamdunr@gmail.com>
2016-09-26 11:11:05 -0400

    SD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
     add_flash_types :error
+
+  config.time_zone = 'Eastern Time (US & Canada)'
+  config.active_record.default_timezone = 'Eastern Time (US & Canada)'
 end
