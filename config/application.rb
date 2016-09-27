4ece4cbbe7dca81f6ba8c46be3a0ef56a8edf596
shyam-ramakrishnan <r.shyamdunr@gmail.com>
2016-09-27 18:43:29 -0400

    changed timezone in application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module Lib
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.default_timezone = :local
+    #config.time_zone = 'Eastern Time (US & Canada)'
+    #config.active_record.default_timezone = :local
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
